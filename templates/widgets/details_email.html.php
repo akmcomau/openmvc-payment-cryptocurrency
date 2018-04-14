cb3215c25f091f504e151bd408a7d5968e05b8e1
Martin Akman <martin@akm.com.au>
2018-04-14 13:30:33 +1000

    Improve email templates

--- templates/widgets/details_email.html.php
+++ templates/widgets/details_email.html.php
@@ -1,16 +1,23 @@
-<table style="background-color: #FFF;width: 100%;padding: 20px; max-width: 500px;">
+<table style="background-color: #FFF;width: 100%;">
 <thead>
 <tr>
 <th colspan="2" style="text-align: left;"><?php echo $text_cryptocurrency; ?></th>
 </tr>
 </thead>
+<tr style="background-color: pink;">
+<th><?php echo $text_deposit_description; ?>: </th>
+<td><?php echo $deposit_description;  ?></td>
+</tr>
+<tr>
+<th colspan="2">&nbsp;</th>
+</tr>
 <?php $cryptos = ['bitcoin', 'ethereum', 'ethereum_classic', 'litecoin', 'bitcoin_cash']; ?>
 <?php foreach ($cryptos as $crypto) { ?>
 <?php if ($module_config->{$crypto}) { ?>
 <tr>
-<th style="text-align: right;border-top: 1px solid #ddd;"><?php echo ${'text_'.$crypto}; ?>: </th>
-<td style="border-top: 1px solid #ddd;">
-<strong><?php echo $module_config->{$crypto}; ?></strong>
+<th><?php echo ${'text_'.$crypto}; ?>: </th>
+<td>
+<?php echo $module_config->{$crypto}; ?>
 <?php if (!empty(${'text_'.$crypto.'_note'})) { ?>
 <br /><br />
 <p class="<?php echo 'text-'.$crypto.'-note'; ?>"><?php echo nl2br(${'text_'.$crypto.'_note'}); ?></p>
@@ -20,8 +27,4 @@
 </tr>
 <?php } ?>
 <?php } ?>
-<tr>
-<th style="text-align: right;border-top: 1px solid #ddd;"><?php echo $text_deposit_description; ?>: </th>
-<td style="border-top: 1px solid #ddd;"><?php echo $deposit_description;  ?></td>
-</tr>
 </table>
--- templates/widgets/details_email.txt.php
+++ templates/widgets/details_email.txt.php
@@ -1,5 +1,7 @@
 <?php echo $text_cryptocurrency; ?>
 
+<?php echo $text_deposit_description; ?>: <?php echo $deposit_description;  ?>
+
 <?php $cryptos = ['bitcoin', 'ethereum', 'ethereum_classic', 'litecoin', 'bitcoin_cash']; ?>
 <?php foreach ($cryptos as $crypto) { ?>
 <?php if ($module_config->{$crypto}) { ?>
